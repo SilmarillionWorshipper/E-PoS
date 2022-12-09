827a63a8b35f03505f3a8fa1e513852b7af5148e
Elijah Miller <silmarillionworshipper@outlook.com>
2022-12-09 08:48:33 +0000

    innit

---
+++ XmasProjectForms/Form1.cs
@@ -0,0 +1,25 @@
+namespace XmasProjectForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("lmao you NERD!");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
