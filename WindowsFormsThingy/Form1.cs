f71cf32a7f48f488eb5a8d7b212b76a832d8a29d
Elijah Miller <SilmarillionWorshipper@outlook.com>
2023-01-08 17:48:46 +0000

    Added file loader

--- WindowsFormsThingy/Form1.cs
+++ WindowsFormsThingy/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Metrics;
+
 namespace WindowsFormsThingy
 {
     public partial class Form1 : Form
@@ -9,7 +11,7 @@ namespace WindowsFormsThingy
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            LoadFile();
         }
 
         private void penButton_Click(object sender, EventArgs e)
@@ -58,6 +60,47 @@ namespace WindowsFormsThingy
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Procedure to load the data stored on Stock.txt into a 2D array
+        private void LoadFile()
+        {
+            // Declares 2D array and a counter for putting items into the array
+            string[,] StockData = new string[11,4];
+            int Counter = 0;
+
+            try
+            {
+                TextReader TextFile = new StreamReader("Stock.txt");
+                string Data = TextFile.ReadLine();
+                while (Data != null)
+                {
+                    Data = TextFile.ReadLine();
+                    // Searches through string to find the first comma, then use that as reference to know what data is stored where
+                    for (int Index = 0; Index < Data.Length; Index++)
+                    {
+                        string Item = Convert.ToString(Data[Index]);
+                        if (Item == ",")
+                        {
+                            StockData[Counter, 0] = Data.Substring(0, Index + 1);
+                            StockData[Counter, 1] = Data.Substring(Index + 2, 2);
+                            StockData[Counter, 2] = Data.Substring(Index + 6, 2);
+                            StockData[Counter, 3] = Data.Substring(Index + 10, 1);
+                        }
+                    }
+                    Counter++;
+                }
+                TextFile.Close();
+            }
+            catch
+            {
+                MessageBox.Show("File error", "Error");
+            }
+        }   
+
+        private void TextBoxLoader()
         {
 
         }
